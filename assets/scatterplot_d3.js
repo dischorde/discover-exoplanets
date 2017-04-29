7bb85bf00630fb4841a41fb8b6d5f4f705cd51b6
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-04-28 18:59:19 -0700

    Add color scale based on composition class

--- assets/scatterplot_d3.js
+++ assets/scatterplot_d3.js
@@ -14,20 +14,22 @@ class Scatterplot {
 
   drawChart(dataset, xKey, yKey) {
     const filteredData = dataset.filter(datapoint => (
-      (datapoint[xKey] !== "" && datapoint[yKey] !== "") &&
-      !(isNaN(datapoint[xKey]) || isNaN(datapoint[yKey]))
-    ));
+      (datapoint[xKey]) && datapoint[yKey]) &&
+      (datapoint[xKey].trim() !== "" && datapoint[yKey].trim() !== "")
+    );
 
     window.filtered = filteredData;
 
     this.setScales(filteredData, xKey, yKey);
+
     this.svg.selectAll("circle")
        .data(filteredData)
        .enter()
        .append("circle")
-       .attr("cx", (d) => this.xScale(Number(d[xKey])))
-       .attr("cy",(d) => this.yScale(Number(d[yKey])))
-       .attr("r", (d) => 2);
+       .attr("fill", d => this.colorScale(d["P. Composition Class"]))
+       .attr("cx", d => this.xScale(Number(d[xKey])))
+       .attr("cy", d => this.yScale(Number(d[yKey])))
+       .attr("r", d => 2);
 
      const xAxis = d3.axisBottom(this.xScale);
 
@@ -57,6 +59,10 @@ class Scatterplot {
                     .range([this.height - this.padding, this.padding])
                     .domain(d3.extent(dataset, (d) => Number(d[yKey])))
                     .nice();
+
+    this.colorScale = d3.scaleOrdinal()
+                      .range(['#85992C','#8C19C1', '#C61C6F', '#B58929', '#268BD2'])
+                      .domain(["water-gas", "gas", "rocky-iron", "rocky-water", "iron"]);
   }
 }
 
